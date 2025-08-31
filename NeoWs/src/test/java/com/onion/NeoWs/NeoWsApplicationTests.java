f79c68fafbe9007b395ed19a494ad9f3255160ba
Aaron Wittchen <ahorn103@gmx.de>
2025-08-31 22:32:11 +0200

    Fixed Tests

--- NeoWs/src/test/java/com/onion/NeoWs/NeoWsApplicationTests.java
+++ NeoWs/src/test/java/com/onion/NeoWs/NeoWsApplicationTests.java
@@ -2,11 +2,26 @@ package com.onion.NeoWs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import com.onion.NeoWs.client.NasaClient;
+import com.onion.NeoWs.event.AsteroidCollisionEvent;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class NeoWsApplicationTests {
+
+	@MockBean
+	private KafkaTemplate<String, AsteroidCollisionEvent> kafkaTemplate;
+
+	@MockBean
+	private NasaClient nasaClient;
+
+	@MockBean
+	private RestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
--- emailnotificationservice/src/test/java/com/onion/emailnotificationservice/EmailnotificationserviceApplicationTests.java
+++ emailnotificationservice/src/test/java/com/onion/emailnotificationservice/EmailnotificationserviceApplicationTests.java
@@ -2,11 +2,54 @@ package com.onion.emailnotificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import com.onion.NeoWs.event.AsteroidCollisionEvent;
+import com.onion.emailnotificationservice.repository.NotificationRepository;
+import com.onion.emailnotificationservice.repository.UserRepository;
+import com.onion.emailnotificationservice.service.EmailContentBuilder;
+import com.onion.emailnotificationservice.service.EmailSenderService;
+import com.onion.emailnotificationservice.service.EmailService;
+import com.onion.emailnotificationservice.service.NasaApodService;
+import com.onion.emailnotificationservice.service.NotificationProcessor;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class EmailnotificationserviceApplicationTests {
+
+	@MockBean
+	private KafkaTemplate<String, AsteroidCollisionEvent> kafkaTemplate;
+
+	@MockBean
+	private EmailService emailService;
+
+	@MockBean
+	private UserRepository userRepository;
+
+	@MockBean
+	private NotificationRepository notificationRepository;
+
+	@MockBean
+	private NotificationProcessor notificationProcessor;
+
+	@MockBean
+	private EmailContentBuilder emailContentBuilder;
+
+	@MockBean
+	private EmailSenderService emailSenderService;
+
+	@MockBean
+	private JavaMailSender javaMailSender;
+
+	@MockBean
+	private NasaApodService nasaApodService;
+
+	@MockBean
+	private RestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
